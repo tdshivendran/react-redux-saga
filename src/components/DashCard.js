af16eb289ad4514276e58e64afd3e8fa9105196b
tdshivendran <tdshivendran@gmail.com>
2019-03-28 14:57:14 -0600

    first commit

--- src/components/DashCard.js
+++ src/components/DashCard.js
@@ -2,11 +2,10 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardHeaderRaw from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import { withStyles } from "@material-ui/core/styles";
 import AvatarRaw from "@material-ui/core/Avatar";
+import DroneData from "./Drone";
+import Weather from "./Weather";
 
 const cardStyles = theme => ({
   root: {
@@ -38,27 +37,10 @@ const NowWhat = props => {
   const { classes } = props;
   return (
     <Card className={classes.card}>
-      <CardHeader title="OK, Shivendran, you're all setup. Now What?" />
-      <CardContent>
-        <List>
-          <ListItem>
-            <Avatar>1</Avatar>
-            <ListItemText primary="Connect to the Drone API" />
-          </ListItem>
-          <ListItem>
-            <Avatar>2</Avatar>
-            <ListItemText primary="Create your Visualization" />
-          </ListItem>
-          <ListItem>
-            <Avatar>3</Avatar>
-            <ListItemText primary="Poll the API" />
-          </ListItem>
-          <ListItem>
-            <Avatar>4</Avatar>
-            <ListItemText primary="Submit Your App" />
-          </ListItem>
-        </List>
-      </CardContent>
+        <CardHeader title="Current Drone Data"/>
+        <CardContent style={{textAlign: "center"}}>
+            <DroneData />
+        </CardContent>
     </Card>
   );
 };
